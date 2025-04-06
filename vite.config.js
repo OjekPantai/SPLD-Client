2e3b3174fc5625e3527c2ae4779ebe8866d4da2a
OjekPantai <elvien.purnawan01@gmail.com>
2025-04-07 03:56:22 +0700

    fix: load environment variables for API URL in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,28 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import path from "path";
 import react from "@vitejs/plugin-react";
 
-const apiUrl = import.meta.env.VITE_API_URL;
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: apiUrl,
-        changeOrigin: true,
-      },
-      "/media": {
-        target: apiUrl,
-        changeOrigin: true,
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        "/api": {
+          target: env.VITE_API_URL,
+          changeOrigin: true,
+        },
+        "/media": {
+          target: env.VITE_API_URL,
+          changeOrigin: true,
+        },
       },
     },
-  },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
+  };
 });
